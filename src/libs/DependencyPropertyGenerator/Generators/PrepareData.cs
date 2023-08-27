9d64d2061c5475673125ef616764c461c891592e
Konstantin S <havendv@gmail.com>
2023-08-27 17:47:39 +0400

    refactor: Removed GeneratorExtensions.cs.

--- src/libs/DependencyPropertyGenerator/Generators/PrepareData.cs
+++ src/libs/DependencyPropertyGenerator/Generators/PrepareData.cs
@@ -238,4 +238,57 @@ public static class PrepareData
             Framework: framework,
             Methods: methods.ToImmutableArray().AsEquatableArray());
     }
+
+    private static bool? IsSpecialType(this ITypeSymbol? symbol)
+    {
+        if (symbol == null)
+        {
+            return null;
+        }
+
+        return
+            symbol is IArrayTypeSymbol ||
+            symbol.SpecialType != SpecialType.None ||
+            (symbol.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T &&
+             symbol.BaseType != null &&
+             symbol.BaseType.SpecialType != SpecialType.None);
+    }
+
+    private static string? GetNamedArgumentExpression(this AttributeSyntax attributeSyntax, string name)
+    {
+        attributeSyntax = attributeSyntax ?? throw new ArgumentNullException(nameof(attributeSyntax));
+
+        return attributeSyntax.ArgumentList?.Arguments
+            .FirstOrDefault(x =>
+            {
+                var nameEquals = x.NameEquals?.ToFullString()
+                    .Trim('=', ' ', '\t', '\r', '\n');
+
+                return nameEquals == name;
+            })?
+            .Expression
+            .ToFullString();
+    }
+    
+    private static string RemoveNameof(this string value)
+    {
+        value = value ?? throw new ArgumentNullException(nameof(value));
+
+        return value.Contains("nameof(")
+            ? value
+                .Substring(value.LastIndexOf('.') + 1)
+                .TrimEnd(')', ' ')
+            : value;
+    }
+
+    internal static AttributeSyntax? TryFindAttributeSyntax(this ClassDeclarationSyntax classSyntax,
+        AttributeData attribute)
+    {
+        var name = attribute.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString();
+
+        return classSyntax.AttributeLists
+            .SelectMany(static x => x.Attributes)
+            .FirstOrDefault(
+                x => x.ArgumentList?.Arguments.FirstOrDefault()?.ToString().Trim('"').RemoveNameof() == name);
+    }
 }
